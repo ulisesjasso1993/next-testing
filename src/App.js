3655b99e3774fa4f14382643298cd08d7dc41622
Jordi <ulisesjasso66@gmail.com}>
2024-02-18 22:35:46 -0600

    start of next.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import LikeButton from "./LikeButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header({title}) {
+    return <h1>{title}</h1>;
 }
 
-export default App;
+export default function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    return (
+        <div>
+            <Header title="React"/>
+            <ul>
+                {names.map((name) => (
+                        <li key={name}>{name}</li>
+                    )
+                )}
+            </ul>
+            <LikeButton />
+        </div>
+    )
+}
